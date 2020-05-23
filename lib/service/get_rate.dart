5211bcdf3b128faa0bdebecf3a6b42ed1a155e97
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-23 10:25:09 +0100

    GetRate gets rate for all crypto type

--- lib/service/get_rate.dart
+++ lib/service/get_rate.dart
@@ -5,56 +5,24 @@ const baseURL = 'https://rest.coinapi.io/v1/exchangerate';
 const apiKey = 'BC3D5815-2462-41A3-927C-AF58924114EB';
 
 class GetRate {
+  GetRate({this.currency});
+
+  final String currency;
+  double ethRate;
+  double bctRate;
+  double ltcRate;
+
   Future getCryptRate({String crypto, String currency}) async {
+    ethRate =
+        await getRateByCryptoAndCurrency(crypto: 'ETH', currency: currency);
+    bctRate =
+        await getRateByCryptoAndCurrency(crypto: 'BTC', currency: currency);
+    ltcRate =
+        await getRateByCryptoAndCurrency(crypto: 'LTC', currency: currency);
+  }
+
+  Future getRateByCryptoAndCurrency({String crypto, String currency}) async {
     var url = '$baseURL/$crypto/$currency';
-
-    var response = await http.get(
-      url,
-      headers: {'X-CoinAPI-Key': apiKey},
-    );
-
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      return jsonResponse['rate'];
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
-    }
-  }
-
-  Future getBTC({String crypto, String currency}) async {
-    var url = '$baseURL/BTC/$currency';
-
-    var response = await http.get(
-      url,
-      headers: {'X-CoinAPI-Key': apiKey},
-    );
-
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      return jsonResponse['rate'];
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
-    }
-  }
-
-  Future getETH({String crypto, String currency}) async {
-    var url = '$baseURL/ETH/$currency';
-
-    var response = await http.get(
-      url,
-      headers: {'X-CoinAPI-Key': apiKey},
-    );
-
-    if (response.statusCode == 200) {
-      var jsonResponse = convert.jsonDecode(response.body);
-      return jsonResponse['rate'];
-    } else {
-      print('Request failed with status: ${response.statusCode}.');
-    }
-  }
-
-  Future getLTC({String crypto, String currency}) async {
-    var url = '$baseURL/LTC/$currency';
 
     var response = await http.get(
       url,
