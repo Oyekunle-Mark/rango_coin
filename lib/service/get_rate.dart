24c3b9ce5e8db9e7c68448c4637f17d020c038b8
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-23 09:24:19 +0100

    Gets the current rate

--- lib/service/get_rate.dart
+++ lib/service/get_rate.dart
@@ -1,3 +1,20 @@
-import 'dart:convert';
+import 'dart:convert' as convert;
+import 'package:http/http.dart' as http;
 
-class GetRate {}
+const baseURL = 'https://rest.coinapi.io/v1/exchangerate';
+
+class GetRate {
+  Future getCurrentRate(crypto, currency) async {
+    var url = '$baseURL/$crypto/$currency';
+
+    // Await the http get response, then decode the json-formatted response.
+    var response = await http.get(url);
+
+    if (response.statusCode == 200) {
+      var jsonResponse = convert.jsonDecode(response.body);
+      return jsonResponse['rate'];
+    } else {
+      print('Request failed with status: ${response.statusCode}.');
+    }
+  }
+}
