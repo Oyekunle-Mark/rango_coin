3793a9259d74fa4704631371febcd00511f357dc
Oyekunle Oloyede <oyekunlemac@gmail.com>
2020-05-23 09:27:07 +0100

    Added the api key to the request header

--- lib/service/get_rate.dart
+++ lib/service/get_rate.dart
@@ -2,13 +2,17 @@ import 'dart:convert' as convert;
 import 'package:http/http.dart' as http;
 
 const baseURL = 'https://rest.coinapi.io/v1/exchangerate';
+const apiKey = 'BC3D5815-2462-41A3-927C-AF58924114EB';
 
 class GetRate {
   Future getCurrentRate(crypto, currency) async {
     var url = '$baseURL/$crypto/$currency';
 
     // Await the http get response, then decode the json-formatted response.
-    var response = await http.get(url);
+    var response = await http.get(
+      url,
+      headers: {'X-CoinAPI-Key': apiKey},
+    );
 
     if (response.statusCode == 200) {
       var jsonResponse = convert.jsonDecode(response.body);
